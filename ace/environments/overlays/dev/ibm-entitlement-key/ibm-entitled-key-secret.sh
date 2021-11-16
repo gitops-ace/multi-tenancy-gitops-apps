e3b173d03799660019c873875167ef146213cbb5
Kok Sing Khong <khongks@gmail.com>
2021-11-17 01:03:03 +1100

    Update ibm-entitlement-key

--- ace/environments/overlays/dev/ibm-entitlement-key/ibm-entitled-key-secret.sh
+++ ace/environments/overlays/dev/ibm-entitlement-key/ibm-entitled-key-secret.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set variables
-IBM_ENTITLEMENT_KEY=<ENTITLEMENT-KEY>
+# IBM_ENTITLEMENT_KEY=<ENTITLEMENT-KEY>
 SEALEDSECRET_NAMESPACE=sealed-secrets
 
 # Create Kubernetes Secret yaml
@@ -9,10 +9,10 @@ oc create secret docker-registry ibm-entitlement-key \
 --docker-username=cp \
 --docker-server=cp.icr.io \
 --docker-password=${IBM_ENTITLEMENT_KEY} \
---dry-run=true -o yaml > delete-ibm-entitled-key-secret.yaml
+--dry-run=client -o yaml > delete-ibm-entitled-key-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-entitled-key-secret.yaml > ibm-entitled-key-secret.yaml
+kubeseal --scope cluster-wide --controller-name=sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-entitled-key-secret.yaml > ibm-entitled-key-secret.yaml
 
 # NOTE, do not check delete-ibm-entitled-key-secret.yaml into git!
 rm delete-ibm-entitled-key-secret.yaml

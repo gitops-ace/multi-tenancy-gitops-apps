a10ccfb589f6b7c6ff4ded3840791ef1094e693f
Kok Sing Khong <khongks@gmail.com>
2021-11-17 00:17:27 +1100

    After ace bootstrap

--- soapserver/environments/soapserver/secrets/ibm-jks-ibm-passwords-secret.sh
+++ soapserver/environments/soapserver/secrets/ibm-jks-ibm-passwords-secret.sh
@@ -11,7 +11,7 @@ oc create secret generic ibm-passwords \
 --from-literal=CLIENT_SSL_TRUST_STORE_PASSWORD=passw0rd \
 --type Opaque \
 --namespace=tools \
---dry-run=true -o yaml > delete-ibm-passwords-secret.yaml
+--dry-run=client -o yaml > delete-ibm-passwords-secret.yaml
 
 
 oc create secret generic ibm-jks \
@@ -19,11 +19,11 @@ oc create secret generic ibm-jks \
 --from-file=ibm-soap-server.jks=/Users/Ritu.Patel@ibm.com/Desktop/ace-prod/github-ace/ace-rest-ws/certs/ibm-soap-server.jks \
 --type Opaque \
 --namespace=tools \
---dry-run=true -o yaml > delete-ibm-jks-secret.yaml
+--dry-run=client -o yaml > delete-ibm-jks-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal -n ${NAMESPACE} --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-jks-secret.yaml > ibm-jks-secret.yaml
-kubeseal -n ${NAMESPACE} --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-passwords-secret.yaml > ibm-passwords-secret.yaml
+kubeseal -n ${NAMESPACE} --controller-name=sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-jks-secret.yaml > ibm-jks-secret.yaml
+kubeseal -n ${NAMESPACE} --controller-name=sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-passwords-secret.yaml > ibm-passwords-secret.yaml
 
 
 # NOTE, do not check delete-ibm-entitled-key-secret.yaml into git!

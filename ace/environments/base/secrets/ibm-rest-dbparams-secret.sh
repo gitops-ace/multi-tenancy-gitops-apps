5ccd23284234ba8d70317b6aa153b3ccf4402a3c
Kok Sing Khong <khongks@gmail.com>
2021-11-17 19:01:59 +1100

    Update dbparms.

--- ace/environments/base/secrets/ibm-rest-dbparams-secret.sh
+++ ace/environments/base/secrets/ibm-rest-dbparams-secret.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set variables
-IBM_REST_DBPARMS_PATH=<IBM-REST-DBPARMS-PATH>
+IBM_REST_DBPARMS_PATH=/Users/khongks/Documents/Dev/gitops-ace/multi-tenancy-gitops-apps/ace/environments/base/secrets
 SEALEDSECRET_NAMESPACE=sealed-secrets
 
 # cat file ibm-rest-dbparms.txt
@@ -14,7 +14,7 @@ SEALEDSECRET_NAMESPACE=sealed-secrets
 oc create secret generic ibm-rest-dbparms.txt \
 --from-file=configuration=${IBM_REST_DBPARMS_PATH}/ibm-rest-dbparms.txt \
 --type Opaque \
---dry-run=true -o yaml > delete-ibm-rest-dbparms-secret.yaml
+--dry-run=client -o yaml > delete-ibm-rest-dbparms-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
 kubeseal --scope cluster-wide --controller-name=sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-rest-dbparms-secret.yaml > ibm-rest-dbparms-secret.yaml

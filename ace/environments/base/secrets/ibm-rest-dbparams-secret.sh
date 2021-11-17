d4500c531093f401c90849b140149ca1ca4081da
Kok Sing Khong <khongks@gmail.com>
2021-11-17 18:35:29 +1100

    Update certs

--- ace/environments/base/secrets/ibm-rest-dbparams-secret.sh
+++ ace/environments/base/secrets/ibm-rest-dbparams-secret.sh
@@ -17,7 +17,7 @@ oc create secret generic ibm-rest-dbparms.txt \
 --dry-run=true -o yaml > delete-ibm-rest-dbparms-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-rest-dbparms-secret.yaml > ibm-rest-dbparms-secret.yaml
+kubeseal --scope cluster-wide --controller-name=sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-rest-dbparms-secret.yaml > ibm-rest-dbparms-secret.yaml
 
 # NOTE, do not check delete-*-secret.yaml into git!
 rm delete-ibm-rest-dbparms-secret.yaml
